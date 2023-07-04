f543a37abf35c274e468a89ee9adebff3064b5b4
youngniiiiii <hanuje_@naver.com>
2023-07-04 11:11:25 +0900

    02.변수 학습

---
+++ client/chater/core/02.variables.js
@@ -0,0 +1,33 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+let admin; //undefined
+let name;
+name = 'John';
+admin = name;
+// console.log(admin);
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+let CalcProductPriceQuantity;
+// - 구매 제품 가격의 총 합
+let totalProductPrices;
+// - 1년 기준 일(day)자 수
+const DAYS_PER_YEAR = 365;
+// - 구매 결제 여부 (했어 안했어 / 있어 없어 ) is 를 붙히는게 관례  / flase true
+let isPayment = true;
+let hasClassName = true; // has 도 마찬가지
+// - 구매 결제 내역
+let paymentHistory;
+// - 브랜드 접두사
+const BRAND_PREFIX = 'NIKE';
+// - 오늘의 운세
+let fortuneOfToday;
+// - 상품 정보
+let productInfo = '';
+
+/* variables ----------------------------------------------------------- */
+
+/* constant variables -------------------------------------------------- */
--- client/main.js
+++ client/main.js
@@ -1,6 +1,2 @@
 /* eslint-disable */
-console.log('hello js!');
-const a = 10;
 /* eslint-disable */
-
-const a = 10;
