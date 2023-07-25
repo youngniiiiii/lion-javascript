c9f38a42b94063b956aa805c0f092fe122e8c44e
youngniiiiii <hanuje_@naver.com>
2023-07-25 14:51:08 +0900

    xhr 콜백 유틸함수 만들기 학습.

--- client/lib/utils/xhr.js
+++ client/lib/utils/xhr.js
@@ -12,63 +12,19 @@
 
 /* callback --------------------------------------------- */
 
-/* function xhr(method, url, onSuccess, onFail, body, headers) {
-  const xhr = new XMLHttpRequest();
-  xhr.open(method, url);
+// 객체 구조 분해 할당
 
-  Object.entries(headers).forEach(([key, value]) => {
-    xhr.setRequestHeader(key, value);
-  });
-
-  xhr.addEventListener('readystatechange', () => {
-    const { status, readyState, response } = xhr;
-    if (readyState === 4) {
-      if (status >= 200 && status < 400) {
-        onSuccess(JSON.parse(response));
-      } else {
-        onFail('실패');
-      }
-    }
-  });
-
-  xhr.send(JSON.stringify(body));
-}
-
-xhr(
-  'POST',
-  'https://jsonplaceholder.typicode.com/users',
-  (result) => {
-    console.log(result);
-  },
-  (err) => {
-    console.log(err);
-  },
-  {
-    name: 'tiger',
-    age: 32,
-  },
-  {
-    'Content-Type': 'application/json',
+export function xhr({
+  method = 'GET',
+  url = '',
+  onSuccess = null,
+  onFail = null,
+  body = null,
+  headers = {
+    'Content-Type': 'application.json',
     'Access-Control-Allow-Origin': '*',
-  }
-);
- */
-
-function xhr(options) {
-  // method, url, onSuccess, onFail, body, headers
-
-  const {
-    method = 'GET',
-    url = '',
-    onSuccess = null,
-    onFail = null,
-    body = null,
-    headers = {
-      'Content-Type': 'application.json',
-      'Access-Control-Allow-Origin': '*',
-    },
-  } = options;
-
+  },
+} = {}) {
   const xhr = new XMLHttpRequest();
   xhr.open(method, url);
 
@@ -91,29 +47,65 @@ function xhr(options) {
 }
 
 // method, url, onSuccess, onFail, body, headers
-xhr({
-  url: 'https://jsonplaceholder.typicode.com/users',
-  onSuccess: () => {},
-  onFail: () => {},
-  body: {
-    name: 'tiger',
-  },
-});
 
-/* 
-'POST',
-'https://jsonplaceholder.typicode.com/users',
-(result) => {
-  console.log(result);
-},
-(err) => {
-  console.log(err);
-},
-{
-  name: 'tiger',
-  age: 32,
-},
-{
-  'Content-Type': 'application/json',
-  'Access-Control-Allow-Origin': '*',
-} */
+// xhr({
+//   method:'PUT',
+//   url:'https://jsonplaceholder.typicode.com/users',
+//   onSuccess(result){
+//     console.log( result );
+//   },
+//   onFail(err){
+//     console.log( err );
+//   },
+//   body:{
+//     name:'tiger'
+//   },
+// });
+
+// 1. 자바스크립트의 함수는 객체다.
+// 2. 사용자(협업개발자) 입장 : 쉽게 쓰자
+// 3. 설계자 -> 함수 안에 메서드(객체)를 넣어 버리자 !!
+
+/**
+ *
+ * @param {string} url 서버와 통신할 주소
+ * @param {function} onSuccess 서버와 통신 성공시 실행될 콜백 함수
+ * @param {function} onFail 서버와의 통신 실패시 실행될 콜백 함수
+ * @return server data
+ */
+xhr.get = (url, onSuccess, onFail) => {
+  xhr({
+    url,
+    onSuccess,
+    onFail,
+  });
+};
+
+xhr.post = (url, body, onSuccess, onFail) => {
+  xhr({
+    method: 'POST',
+    url,
+    body,
+    onSuccess,
+    onFail,
+  });
+};
+
+xhr.put = (url, body, onSuccess, onFail) => {
+  xhr({
+    method: 'PUT',
+    url,
+    body,
+    onSuccess,
+    onFail,
+  });
+};
+
+xhr.delete = (url, onSuccess, onFail) => {
+  xhr({
+    method: 'DELETE',
+    url,
+    onSuccess,
+    onFail,
+  });
+};
--- client/main.js
+++ client/main.js
@@ -1,1 +1,6 @@
-import {} from './lib/index.js';
+import { xhr } from './lib/index.js';
+
+// xhr.get(주소넣고);
+// .then 성공하면 여기 실행
+// .catch 실패하면 여기 실행
+// 이렇게 만들고 싶어   => 콜백이 불편해서 나온게 Promise
